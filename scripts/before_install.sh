96ee032a1ad22363d3899f0272f045d2d7afbb2e
vikramveer127 <124433181+vikramveer127@users.noreply.github.com>
2023-03-24 18:51:18 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,7 +8,7 @@ sudo apt-get -y update
 sudo apt-get -y install ruby
 sudo apt-get -y install wget
 cd /home/ubuntu
-wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+wget https://aws-codedeploy-ap-south-1.s3.amazonaws.com/latest/install
 sudo chmod +x ./install 
 sudo ./install auto
 
